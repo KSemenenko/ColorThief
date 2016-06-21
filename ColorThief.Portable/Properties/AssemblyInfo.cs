745b3a8cbc0984772b3fc3e644580c94bfdfc07c
ksemenenko <mail@ksemenenko.com>
2016-06-21 15:27:25 +0300

    *

--- ColorThief.Portable/Properties/AssemblyInfo.cs
+++ ColorThief.Portable/Properties/AssemblyInfo.cs
@@ -1,5 +1,1 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-
-
+﻿
--- ColorThief.iOS/Properties/AssemblyInfo.cs
+++ ColorThief.iOS/Properties/AssemblyInfo.cs
@@ -1,14 +1,11 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-
+﻿using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d778f4be-e4bf-436d-b4a1-5de48a6f785f")]
 
+[assembly: Guid("d778f4be-e4bf-436d-b4a1-5de48a6f785f")]
