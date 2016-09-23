13572a0c88e0aaa09eef0c50f5bf178ea6c1f226
Kostiantyn Semenenko <kostiantyn.semenenko@e-consulting.com.ua>
2016-09-23 17:55:49 +0300

    mobile

--- ColorTestApp/ColorTestApp/App.cs
+++ ColorTestApp/ColorTestApp/App.cs
@@ -2,42 +2,89 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using CoreGraphics;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 using Xamarin.Forms;
 
 namespace ColorTestApp
 {
-	public class App : Application
-	{
-		public App ()
-		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							HorizontalTextAlignment = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
-		}
+    //http://stackoverflow.com/questions/28123300/xamarin-forms-c-sharp-find-dominant-color-of-image-or-image-byte-array
+    //http://stackoverflow.com/questions/7807360/how-to-get-pixel-colour-in-android
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+    public class App : Application
+    {
+        public App ()
+        {
+            // The root page of your application
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+            var takePhoto = new Button() { Text = "GetPhoto"};
+            var image = new Image();
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+            takePhoto.Clicked += async (sender, args) =>
+            {
+                MediaFile file;
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    file = await CrossMedia.Current.PickPhotoAsync();
+                }
+                else
+                {
+                    file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                    {
+                        Directory = "Sample",
+                        Name = "test.jpg"
+                    });
+                }
+               
+
+                if (file == null)
+                    return;
+
+
+                image.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    file.Dispose();
+                    return stream;
+                });
+
+
+               // ColorThief.ColorThief ct = new ColorThief.ColorThief();
+              
+
+            };
+
+
+            MainPage = new ContentPage {
+                Content = new StackLayout {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        },
+                        takePhoto,
+                        image
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart ()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep ()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume ()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
