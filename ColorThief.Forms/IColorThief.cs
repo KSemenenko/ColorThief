e2f55643713b3a7a1dddab917fab9441213951aa
Konstantin Semenenko <ksemenenko@live.com>
2016-10-07 07:15:38 +0300

    interface

---
+++ ColorThief.Forms/IColorThief.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace ColorThief.Forms
+{
+    public interface IColorThief
+    {
+        /// <summary>
+        ///     Use the median cut algorithm to cluster similar colors and return the base color from the largest cluster.
+        /// </summary>
+        /// <param name="sourceImage">The source image.</param>
+        /// <param name="quality">
+        ///     1 is the highest quality settings. 10 is the default. There is
+        ///     a trade-off between quality and speed. The bigger the number,
+        ///     the faster a color will be returned but the greater the
+        ///     likelihood that it will not be the visually most dominant color.
+        /// </param>
+        /// <param name="ignoreWhite">if set to <c>true</c> [ignore white].</param>
+        /// <returns></returns>
+        QuantizedColor GetColor(ImageSource sourceImage, int quality, bool ignoreWhite);
+
+
+        /// <summary>
+        ///     Use the median cut algorithm to cluster similar colors.
+        /// </summary>
+        /// <param name="sourceImage">The source image.</param>
+        /// <param name="colorCount">The color count.</param>
+        /// <param name="quality">
+        ///     1 is the highest quality settings. 10 is the default. There is
+        ///     a trade-off between quality and speed. The bigger the number,
+        ///     the faster a color will be returned but the greater the
+        ///     likelihood that it will not be the visually most dominant color.
+        /// </param>
+        /// <param name="ignoreWhite">if set to <c>true</c> [ignore white].</param>
+        /// <returns></returns>
+        /// <code>true</code>
+        List<QuantizedColor> GetPalette(ImageSource sourceImage, int colorCount, int quality, bool ignoreWhite)
+
+    }
+}
