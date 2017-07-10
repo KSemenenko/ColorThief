f83d06cc77cff4e3fa3d60c00c90fadd880c6e0b
ksemenenko <ksemenenko@live.com>
2017-07-10 12:12:33 +0300

    try fix #6

--- ColorThief.iOS/ColorThief.cs
+++ ColorThief.iOS/ColorThief.cs
@@ -83,6 +83,51 @@ namespace ColorThiefDotNet
             }
         }
 
+        private byte[][] ConvertPixelsiOS(byte[] pixels, int pixelCount, int quality, bool ignoreWhite)
+        {
+
+
+            var expectedDataLength = pixelCount * ColorDepth;
+            if (expectedDataLength != pixels.Length)
+            {
+                throw new ArgumentException("(expectedDataLength = "
+                                            + expectedDataLength + ") != (pixels.length = "
+                                            + pixels.Length + ")");
+            }
+
+            // Store the RGB values in an array format suitable for quantize
+            // function
+
+            // numRegardedPixels must be rounded up to avoid an
+            // ArrayIndexOutOfBoundsException if all pixels are good.
+
+            var numRegardedPixels = (pixelCount + quality - 1) / quality;
+
+            var numUsedPixels = 0;
+            var pixelArray = new byte[numRegardedPixels][];
+
+            for (var i = 0; i < pixelCount; i += quality)
+            {
+                var offset = i * ColorDepth;
+                var r = pixels[offset];
+                var g = pixels[offset + 1];
+                var b = pixels[offset + 2];
+                var a = pixels[offset + 3];
+
+                // If pixel is mostly opaque and not white
+                if (a >= 125 && !(ignoreWhite && r > 250 && g > 250 && b > 250))
+                {
+                    pixelArray[numUsedPixels] = new[] { r, g, b };
+                    numUsedPixels++;
+                }
+            }
+
+            // Remove unused pixels from the array
+            var copy = new byte[numUsedPixels][];
+            Array.Copy(pixelArray, copy, numUsedPixels);
+            return copy;
+        }
+
         private byte[][] GetPixelsFast(UIImage sourceImage, int quality, bool ignoreWhite)
         {
             if(quality < 1)
@@ -92,7 +137,7 @@ namespace ColorThiefDotNet
             var pixels = GetIntFromPixel(sourceImage);
             var pixelCount = (int)sourceImage.Size.Width * (int)sourceImage.Size.Height;
 
-            return ConvertPixels(pixels, pixelCount, quality, ignoreWhite);
+            return ConvertPixelsiOS(pixels, pixelCount, quality, ignoreWhite);
         }
     }
 }
