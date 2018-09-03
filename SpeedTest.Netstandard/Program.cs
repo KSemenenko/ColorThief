9a4f1ec60cbb67c1303b7810e62b1892616be5f0
a.politov <a.politov@rambler-co,.ru>
2018-09-03 10:49:37 +0300

    netstandard-support

---
+++ SpeedTest.Netstandard/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace SpeedTest.Netstandard
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var x = new TestClass();
+            x.Image1();
+            x.Image2();
+
+            x.Image1();
+            x.Image2();
+
+            x.Image1();
+            x.Image2();
+
+            x.Image1();
+            x.Image2();
+
+            x.Image1();
+            x.Image2();
+
+            x.Image1();
+            x.Image2();
+
+            x.Image1();
+            x.Image2();
+
+            var summary = BenchmarkRunner.Run<TestClass>();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SpeedTest.Netstandard/TestClass.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+using ColorThiefDotNet;
+
+namespace SpeedTest.Netstandard
+{
+    public class TestClass
+    {
+        [Benchmark]
+        public void Image1()
+        {
+            var colorThief = new ColorThief();
+
+            var test = Directory.GetCurrentDirectory();
+            var bitmap = (Bitmap)Image.FromFile("test1.jpg");
+            var result = colorThief.GetColor(bitmap);
+        }
+
+        [Benchmark]
+        public void Image2()
+        {
+            var colorThief = new ColorThief();
+            var bitmap = (Bitmap)Image.FromFile("test2.jpg");
+            var result = colorThief.GetColor(bitmap);
+        }
+    }
+}
